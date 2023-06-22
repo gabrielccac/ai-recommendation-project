684bc479889147cb603beb10aaef4615581c909f
yantavares <yantdo1@gmail.com>
2023-06-22 13:17:04 -0300

    fixed merge issues

--- src/components/ChatWindow/index.tsx
+++ src/components/ChatWindow/index.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 
 import sendIcon from "../../assets/send.svg";
 import { Container, Message } from "./styles";
-import { useState, useRef } from 'react';
 
 import { UserRequest } from "../UserRequest";
 import { AIResponse } from "../AIResponse";
@@ -15,12 +14,22 @@ interface Message {
   response: string;
 }
 
-interface ChatWindowProps {
-  input: string;
-  setInput: (input: string) => void;
-}
+export function ChatWindow() {
+  const [input, setInput] = useState("");
+  const [request, setRequest] = useState("");
+  const [messages, setMessages] = useState([
+    { request: "Hello!", response: "Nahhh" },
+  ]);
 
-export function ChatWindow({ input, setInput }: ChatWindowProps) {
+  useEffect(() => {
+    if (request) {
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { request: request, response: "AI Response " + request },
+      ]);
+    }
+  }, [request, setMessages]);
+
   return (
     <Container>
       <div className="messages">
@@ -42,12 +51,6 @@ export function ChatWindow({ input, setInput }: ChatWindowProps) {
           setInput("");
         }}
       >
-        action='#'
-        className='request-container'
-        onSubmit={() => {
-          console.log(input);
-          setInput('');
-        }}>
         <input
           className="request-input"
           type="text"
